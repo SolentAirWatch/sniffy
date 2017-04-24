1a44e5a1c0ae5aa8398d2f0e85d893dcb4ec41e2
joshctaylor <joshua.taylor312@gmail.com>
2017-04-24 20:54:07 +0000

    install script

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install -y adafruit-mcp3008
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y npm
 sudo apt-get install -y upstart
+pip install paho-mqtt
+pip install pyserial
+pip install Adafruit_GPIO.SPI
+pip install adafruit-mcp3008
+pip install spidev
+
