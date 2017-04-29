5c0516db76e5c26c7cb274e0ea5b9e10eda363c1
hantsairquality <hantsairquality@users.noreply.github.com>
2017-04-30 00:57:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y npm
 sudo apt-get install -y upstart
-pip install paho-mqtt
-pip install pyserial
-pip install Adafruit_GPIO.SPI
-pip install adafruit-mcp3008
-pip install spidev
+sudo pip install paho-mqtt
+sudo pip install pyserial
+sudo pip install Adafruit_GPIO.SPI
+sudo pip install adafruit-mcp3008
+sudo pip install spidev
 
