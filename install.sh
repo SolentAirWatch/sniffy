14057d045b7420f8a0bd9caae2af41f4b1158ea0
hantsairquality <hantsairquality@users.noreply.github.com>
2017-04-30 21:35:03 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
 mkdir ~/AirQuality
 mkdir ~/AirQuality/client
+cd
+virtualenv -p python2 py2Sniffy
+source ./py2Sniffy/bin/activate
 sudo apt-get update
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt-get install -y nodejs
@@ -9,7 +11,5 @@ sudo apt-get install -y npm
 sudo apt-get install -y upstart
 sudo pip install paho-mqtt
 sudo pip install pyserial
-sudo pip install Adafruit_GPIO.SPI
 sudo pip install adafruit-mcp3008
 sudo pip install spidev
-
