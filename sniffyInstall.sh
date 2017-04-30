5360537e7bcf478c3d7e2695678e0b84ee3e45af
hantsairquality <hantsairquality@users.noreply.github.com>
2017-04-30 23:52:43 +0100

    Update and rename install.sh to sniffyInstall.sh
    remove java from monitor install

--- sniffyInstall.sh
+++ sniffyInstall.sh
@@ -6,9 +6,6 @@ virtualenv -p python2 py2Sniffy
 source ./py2Sniffy/bin/activate
 sudo apt-get update
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo apt-get install -y npm
-sudo apt-get install -y upstart
 sudo pip install paho-mqtt
 sudo pip install pyserial
 sudo pip install adafruit-mcp3008
