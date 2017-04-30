3d311797ce9ba7e5f32d54e6b8bb62f4b060369d
hantsairquality <hantsairquality@users.noreply.github.com>
2017-04-30 23:56:34 +0100

    Update sniffyInstall.sh

--- sniffyInstall.sh
+++ sniffyInstall.sh
@@ -5,7 +5,6 @@ cd
 virtualenv -p python2 py2Sniffy
 source ./py2Sniffy/bin/activate
 sudo apt-get update
-curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo pip install paho-mqtt
 sudo pip install pyserial
 sudo pip install adafruit-mcp3008
