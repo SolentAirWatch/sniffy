43ff823c3d05d7f38d0c9349fe560a53a14bed9b
hantsairquality <hantsairquality@users.noreply.github.com>
2017-05-01 00:32:01 +0100

    Update sniffyInstall.sh

--- sniffyInstall.sh
+++ sniffyInstall.sh
@@ -2,8 +2,6 @@
 mkdir ~/AirQuality
 mkdir ~/AirQuality/client
 cd
-virtualenv -p python2 py2Sniffy
-source ./py2Sniffy/bin/activate
 sudo apt-get update
 sudo pip install paho-mqtt
 sudo pip install pyserial
