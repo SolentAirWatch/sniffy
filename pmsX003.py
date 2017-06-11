4f23972efb123335e4c53c594f48e0970a6e181c
joshctaylor <joshua.taylor312@gmail.com>
2017-06-11 15:16:40 +0100

    repaired typos

--- pmsX003.py
+++ pmsX003.py
@@ -7,20 +7,20 @@ import csv
 from pprint import pprint  # makes data more pretty
 
 # add a config file which is unique to each sniffy.
-sensorID = 1 # Imput a sensor number here 
+sensorID = 2 # Imput a sensor number here 
 
 # setup onboard serial port NB RPi 3 address
 port = serial.Serial('/dev/ttyS0', baudrate=9600, timeout=2.0)
 broker = "mqtt.opensensors.io"  # "46.101.13.195"     # test broker
 topic = "/orgs/solentairwatch/sniffy"
-monitorID = 'SOTON0001'  # id 0 is reserved for test
+monitorID = '2'  # id 0 is reserved for test
 monitorLocation = [50.9262, -1.4092]
 csvFile="/home/pi/AirQuality/client/pm.log" # keep a local copy for debug
+global t
+global ts
 t = 0
 ts = 1
 monitorLocation = [50.9262, -1.4092]
-global t
-global ts
 
 def on_connect(client, userdata, rc):
     print("Connected with result code "+str(rc))
@@ -85,7 +85,7 @@ while True: # PMSx003 sensor by default streams data and non-uniform intervals -
             '$gr50um': ord(rcv[24]) * 256 + ord(rcv[25]),
             '$gr100um': ord(rcv[26]) * 256 + ord(rcv[27])
             }
-        pprint(json.dumps(message))
+        pprint(message)
         client.publish(topic, payload=json.dumps(message), qos=0, retain=False)
         client.loop()
         w.writerow(json.dumps(message))
