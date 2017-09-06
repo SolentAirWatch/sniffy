0188141c8506794e08259e8ea624eca145bfbb11
joshctaylor <joshua.taylor312@gmail.com>
2017-09-07 00:07:36 +0100

    Update sniffy.sh

--- sniffy.sh
+++ sniffy.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 
-sleep 30 # avoid starting the scrip before the network is up
-cd /
-cd home/pi/sniffy
-sudo python standalonePMS.py # pmsX003.py
+sleep 30 # avoid starting the script before the network is up
+cd /home/pi/sniffy
+sudo python pmsX003.py
 # sudo bmp180.py
