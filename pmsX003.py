7529ebf4bb04d764ec5c3bf048e87075086e7a4f
joshctaylor <joshua.taylor312@gmail.com>
2017-09-06 22:26:43 +0100

    includes reconnect & CSV headers

--- pmsX003.py
+++ pmsX003.py
@@ -7,11 +7,11 @@ import csv
 from pprint import pprint  # makes data more pretty
 
 # setup onboard serial port NB RPi 3 address
+monitorID = '2'  # id 0 is reserved for test
+monitorLocation = [50.9262, -1.4092]
 port = serial.Serial('/dev/ttyS0', baudrate=9600, timeout=2.0)
 broker = "mqtt.opensensors.io"  # "46.101.13.195"     # test broker
 topic = "/orgs/solentairwatch/sniffy"
-monitorID = '2'  # id 0 is reserved for test
-monitorLocation = [50.9262, -1.4092]
 csvFile = "PMSx003.csv" # keep a local copy for debug
 
 def on_connect(client, userdata, rc):
@@ -27,11 +27,12 @@ def on_publish(client, userdata, mid):
 
 
 def on_disconnect(client, userdata, rc):
-    # need some ability to error log and reconnect
-    print('disconected with code' + str(rc))
+    if rc !=0:
+        print('unexpected disconect with code' + str(rc))
+        client.reconnect()
     
 
-# function to read a line of serial data
+# function to parse a line of binary data from PMSx003 -- needs updating for python 3
 def read_pm_line(_port):
     rv = b''
     while True:
@@ -44,26 +45,40 @@ def read_pm_line(_port):
                 return rv
 
 # set up objects
-f = open(csvFile,'a')
+f = open(csvFile,'a')  # open the csv file but 'a'ppend it if it already exists
+# Authenticate with opensensors.io
 client = mqtt.Client(client_id="xxxx")
 client.username_pw_set("solentairwatch", password="xxxx")
+# set up callbacks
 client.on_connect = on_connect
 client.on_publish = on_publish
 client.connect(broker)  # (address, port, timeout (sec) )
-client.loop()
 
+
+# write a blank message to CSV so we can add headers at the start of the file
+message = {
+    'time': "init",
+    'id': monitorID,
+    'cityName': "",
+    'stationName': "",
+    'averaging': 0,
+    'latitude': monitorLocation[0],
+    'longitude': monitorLocation[1],
+    'PM1': 0,
+    'PM25': 0,
+    'PM10': 0}
+w = csv.DictWriter(f, message.keys())
+w.writeheader()
+
+client.loop_start()  # start the network loop
 
 while True: # PMSx003 sensor by default streams data and non-uniform intervals - replace with timed polling
     try:
-        #print("trying to read")
         rcv = read_pm_line(port)
-        # print("is reading")
-        #  The following needs updating to work on python 3
         message = {
             'time': str(datetime.datetime.now()),
             'id': monitorID,
             'cityName': "Southampton",
-            'stationName': "Common#1",
             'latitude': monitorLocation[0],
             'longitude': monitorLocation[1],
             'averaging': 0,
@@ -82,12 +97,9 @@ while True: # PMSx003 sensor by default streams data and non-uniform intervals -
             }
         # pprint(message)
         client.publish(topic, payload=json.dumps(message), qos=0, retain=False)
-        client.loop()
         w = csv.DictWriter(f, message.keys())
         w.writerow(message)
-        
         time.sleep(0.1) # wait 100 millisonds
-
     except KeyboardInterrupt:
         break
         
