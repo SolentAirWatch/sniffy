23acfc64449ab1938494a4e081402871b3d12bd0
joshctaylor <joshua.taylor312@gmail.com>
2017-11-15 20:11:09 +0000

    Update pmsX003.py

--- pmsX003.py
+++ pmsX003.py
@@ -17,7 +17,7 @@ clientID = 'sniffy-6'
 monitorID = '6'  # id 0 is reserved for test
 monitorLocation = [50.9262, -1.4092]
 port = serial.Serial('/dev/ttyS0', baudrate=9600, timeout=2.0)
-broker = "139.162.227.142" #"mqtt.opensensors.io"  # "46.101.13.195"     # test broker
+broker = "" #"mqtt.opensensors.io"  # "46.101.13.195"     # test broker
 topic = "mqtt-data" #"/orgs/solentairwatch/sniffy"
 csvFile = "PMSx003.csv" # keep a local copy for debug
 
@@ -62,7 +62,7 @@ def readBME():
 f = open(csvFile,'a')  # open the csv file but 'a'ppend it if it already exists
 # Authenticate with opensensors.io
 client = mqtt.Client(client_id=clientID)
-client.username_pw_set("mqtt-user", password="mqtt-password" )
+client.username_pw_set("username", password="password" )
 # set up callbacks
 client.on_connect = on_connect
 client.on_publish = on_publish
