b20d9ee24de99641f6c77be9ddb0191aa3470101
joshctaylor <joshua.taylor312@gmail.com>
2018-05-26 18:06:04 +0100

    1

---
+++ subscribeMQTT.py
@@ -0,0 +1,59 @@
+#    this script forms part of solent air watch data back end
+#    it subcribes to an MQTT broker and adds the data to a sqlite database
+#    based on example code from https://github.com/eclipse/paho.mqtt.python/tree/master/examples
+
+#    Copyright (C) {2017}  {Joshua Taylor - Solent Air Watch}
+
+import paho.mqtt.client as mqtt
+import json
+from pprint import pprint  # makes data more pretty
+
+
+
+global cursor  # make the database handle available globally
+DB_Name = "airwatchData.db"
+topicName = "/sniffy/#"
+username = 'data'
+password = 'IReallyLikeNO2!'
+host = 'awdrop'
+
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, rc):
+    print("Connected with result code "+str(rc))
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe(topicName, qos=0)
+
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    data = (msg.payload.decode('utf-8'))
+    # json.loads
+    pprint(data)
+    # parse the data to the sql database
+##    cursor.execute('''INSERT INTO sniffy(id, timestmp, latitude, longitude, PM10, PM25, PM1)
+##                  VALUES(?,?,?,?,?,?,?)''', (data["id"], data["time"], data["latitude"],
+##                                             data["longitude"], data["PM10"], data["PM25"], data["PM1"]))
+##    db.commit()
+
+client = mqtt.Client()
+client.username_pw_set(username, password=password)
+
+# set MQTT call back functions
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect(host)  # (address, port, timeout (sec) )
+
+# set up database connection
+##db = sqlite3.connect(DB_Name)
+##cursor = db.cursor()
+##cursor.execute('''
+##    CREATE TABLE IF NOT EXISTS  sniffy(id TEXT, timestmp TEXT,
+##                       latitude TEXT, longitude TEXT, PM10 TEXT, PM25 TEXT, PM1 TEXT)
+##''')
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+client.loop_forever()
+db.close()  # this will never be executed because of the forever loop - need some exit logic
--- testMQTTinflux.py
+++ testMQTTinflux.py
@@ -9,8 +9,7 @@ from influxdb import InfluxDBClient
 
 username = 'data'
 password = 'IReallyLikeNO2!'
-broker = 'localhost'
-
+host = 'awdrop'
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
@@ -21,20 +20,27 @@ def on_message(client, userdata, msg):
     receiveTime = datetime.datetime.utcnow()  # Use utc as timestamp
     data = json.loads(msg.payload.decode('utf-8'))  # decode the json message 
     isfloatValue=False
+    #pprint(data)
+    print(data["PM1"])
+    num = data["PM1"]
+    print(isinstance(num, float))
 
+##    cursor.execute('''INSERT INTO sniffy
+
+    jsonData = [
+        {
+            "id": data["id"],
+            "time": data["sendTime"],
+            "fields":{
+                "PM10": data["PM10"],
+                "PM25": data["PM25"],
+                "PM1": data["PM1"]
+                }
+            }
+        ]
+    
     print(str(receiveTime) + ": " + msg.topic + " " + str(data))
-
-#        json_body = [
-#            {
-#                "measurement": msg.topic,
-#                "time": receiveTime,
-#                "fields": {
-#                    "value": val
-#                }
-#            }
-#        ]
-    dbclient.write_points(data)
-    print("Finished writing to InfluxDB")
+    dbclient.write_points(jsonData)
         
 # Set up a client for InfluxDB
 dbclient = InfluxDBClient('localhost', 8086, 'root', 'root', 'sensordata')
@@ -45,6 +51,8 @@ client.username_pw_set(username, password=password)
 client.on_connect = on_connect
 client.on_message = on_message
 connOK=False
+
+# Try connecting, if failed try again after 2 seconds
 while(connOK == False):
     try:
         client.connect(broker)
